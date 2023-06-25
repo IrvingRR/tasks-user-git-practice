edc5d50e754cfbb53231d081c8352314c9190a38
Irving Rodriguez <irvingrodriguezromero@gmail.com>
2023-06-25 13:37:25 -0600

    Vuex implemented

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+import { routes } from './routes';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      // component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+  routes: routes
+});
 
-export default router
+export default router;
---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import { createStore } from 'vuex';
+
+// Create a new store instance.
+export const store = createStore({
+  state () {
+    return {
+        user: {},
+        tasks: []
+    }
+  },
+  mutations: {
+    
+  },
+
+  actions: {
+
+  }
+});
